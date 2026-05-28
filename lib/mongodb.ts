8e142e4ea789e24631fd32c21de559c46a0dc6de
Dappii_ <wongeselep@gmail.com>
2026-05-28 12:53:00 +0700

    fix realtime synsssc

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
+  throw new Error("Please define the MONGODB_URI");
 }
 
 let cached = (global as any).mongoose;
@@ -21,8 +21,8 @@ export async function connectDB() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: "portfolio",
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
     });
   }
 
